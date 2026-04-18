d9c7d97133fb12549a2cf0e8114b0c24caa6e6c5
andre <72755764+drewjya@users.noreply.github.com>
2026-04-18 10:23:08 +0700

    Publish version 1.0

--- composeApp/src/commonMain/kotlin/com/vorkase/vorflow/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.vorkase.vorflow
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/jvmMain/kotlin/com/vorkase/vorflow/UpdateInfo.jvm.kt
+++
@@ -1,3 +0,0 @@
-package com.vorkase.vorflow
-
-actual fun showUpdateInfo(): Boolean = true
